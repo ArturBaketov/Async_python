cb6b7a160af50d0591439d9f6eea45ad2fe69e45
Artur <artur.backetov@yandex.ru>
2025-01-05 21:30:39 +0300

    python async

--- async_await_two.py
+++ async_await_two.py
@@ -1,3 +1,6 @@
+from fileinput import filename
+from nt import write
+
 import requests
 from time import time
 
@@ -17,10 +20,36 @@ def main():
         write_file(get_file(url))
     print(time() - t0)
 
-if __name__ == '__main__':
-    main()
+#if __name__ == '__main__':
+ #   main()
 
 ###########################################################################
 
 import asyncio
 import aiohttp
+
+def write_image(data):
+    filename1 = 'file-{}.jpeg'.format(int(time() * 1000))
+    with open(filename1, 'wb') as file:
+        file.write(data)
+
+async def fetch_content(url, session):
+    async with session.get(url, allow_redirects=True) as response:
+        data = await response.read()
+        write_image(data)
+
+async def main2():
+    url = 'https://loremflickr.com/320/240'
+    tasks1 = []
+
+    async with asyncio.ClientSession() as session:
+        for i in range(10):
+            task = asyncio.create_task(fetch_content(url, session))
+            tasks1.append(task)
+        await asyncio.gather(*tasks1)
+
+if __name__ == '__main__':
+    t0 = time()
+    asyncio.run(main2())
+    print(time() - t0)
+
